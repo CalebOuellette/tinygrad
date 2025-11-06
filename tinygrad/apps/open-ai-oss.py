eaab11a9975521dcc9439e9db6dc06c8ff5ab0b7
Caleb Ouellette <caleb.ouellette@gmail.com>
2025-11-05 23:16:05 -0800

    remove torch.

--- tinygrad/apps/open-ai-oss.py
+++ tinygrad/apps/open-ai-oss.py
@@ -4,9 +4,9 @@ import math
 import os
 from dataclasses import dataclass
 
+from typing import Tuple
 import sys, argparse, typing, re, unicodedata
 from tinygrad import Tensor, nn, UOp, TinyJit, getenv, helpers
-import math
 
 
 @dataclass
@@ -33,58 +33,40 @@ def build_config_from_remote():
   return ModelConfig()
 
 
-class RMSNorm(torch.nn.Module):
-  def __init__(self, num_features: int, eps: float = 1e-05, device: torch.device | None = None):
-    super().__init__()
-    self.num_features = num_features
-    self.eps = eps
-    self.scale = torch.nn.Parameter(torch.ones(num_features, device=device, dtype=torch.float32))
-
-  def forward(self, x: torch.Tensor) -> torch.Tensor:
-    assert x.shape[-1] == self.num_features
-    t, dtype = x.float(), x.dtype
-    t = t * torch.rsqrt(torch.mean(t**2, dim=-1, keepdim=True) + self.eps)
-    return (t * self.scale).to(dtype)
-
-
 def _apply_rotary_emb(
-  x: torch.Tensor,
-  cos: torch.Tensor,
-  sin: torch.Tensor,
-) -> torch.Tensor:
-  cos = cos.unsqueeze(-2).to(x.dtype)
-  sin = sin.unsqueeze(-2).to(x.dtype)
-  x1, x2 = torch.chunk(x, 2, dim=-1)
+  x: Tensor,
+  cos: Tensor,
+  sin: Tensor,
+) -> Tensor:
+  cos = cos.unsqueeze(-2)
+  sin = sin.unsqueeze(-2)
+  x1, x2 = x.chunk(2, dim=-1)
   o1 = x1 * cos - x2 * sin
   o2 = x2 * cos + x1 * sin
-  return torch.cat((o1, o2), dim=-1)
+  return o1.cat(o2, dim=-1)
 
 
-class RotaryEmbedding(torch.nn.Module):
+class RotaryEmbedding:
   def __init__(
     self,
     head_dim: int,
     base: int,
-    dtype: torch.dtype,
     initial_context_length: int = 4096,
     scaling_factor: float = 1.0,
     ntk_alpha: float = 1.0,
     ntk_beta: float = 32.0,
-    device: torch.device | None = None,
   ) -> None:
     super().__init__()
     self.head_dim = head_dim
     self.base = base
-    self.dtype = dtype
     self.initial_context_length = initial_context_length
     self.scaling_factor = scaling_factor
     self.ntk_alpha = ntk_alpha
     self.ntk_beta = ntk_beta
-    self.device = device
 
-  def _compute_concentration_and_inv_freq(self) -> torch.Tensor:
+  def _compute_concentration_and_inv_freq(self) -> Tuple[float, Tensor]:
     """See YaRN paper: https://arxiv.org/abs/2309.00071"""
-    freq = self.base ** (torch.arange(0, self.head_dim, 2, dtype=torch.float, device=self.device) / self.head_dim)
+    freq = self.base ** (Tensor.arange(0, self.head_dim, 2) / self.head_dim)
     if self.scaling_factor > 1.0:
       concentration = 0.1 * math.log(self.scaling_factor) + 1.0  # YaRN concentration
 
@@ -97,7 +79,7 @@ class RotaryEmbedding(torch.nn.Module):
       interpolation = 1.0 / (self.scaling_factor * freq)
       extrapolation = 1.0 / freq
 
-      ramp = (torch.arange(d_half, dtype=torch.float32, device=freq.device) - low) / (high - low)
+      ramp = (Tensor.arange(d_half, device=freq.device) - low) / (high - low)
       mask = 1 - ramp.clamp(0, 1)
 
       inv_freq = interpolation * (1 - mask) + extrapolation * mask
@@ -109,19 +91,19 @@ class RotaryEmbedding(torch.nn.Module):
 
   def _compute_cos_sin(self, num_tokens: int):
     concentration, inv_freq = self._compute_concentration_and_inv_freq()
-    t = torch.arange(num_tokens, dtype=torch.float32, device=self.device)
-    freqs = torch.einsum("i,j->ij", t, inv_freq)
+    t = Tensor.arange(num_tokens)
+    freqs = Tensor.einsum("i,j->ij", t, inv_freq)
     cos = freqs.cos() * concentration
     sin = freqs.sin() * concentration
     return cos, sin
 
   def forward(
     self,
-    query: torch.Tensor,
-    key: torch.Tensor,
-  ) -> tuple[torch.Tensor, torch.Tensor]:
+    query: Tensor,
+    key: Tensor,
+  ) -> tuple[Tensor, Tensor]:
     num_tokens = query.shape[0]
-    cos, sin = self._compute_cos_sin(num_tokens)
+    cos, sin = self._compute_cos_sin(int(num_tokens))
 
     query_shape = query.shape
     query = query.view(num_tokens, -1, self.head_dim)
@@ -146,22 +128,21 @@ def sdpa(Q, K, V, S, sm_scale, sliding_window=0):
   mask = torch.triu(Q.new_full((n_tokens, n_tokens), -float("inf")), diagonal=1)
   if sliding_window > 0:
     mask += torch.tril(mask.new_full((n_tokens, n_tokens), -float("inf")), diagonal=-sliding_window)
-  QK = torch.einsum("qhmd,khmd->hmqk", Q, K)
-  QK *= sm_scale
+  QK = Tensor.einsum("qhmd,khmd->hmqk", Q, K)
+  QK = QK * sm_scale
   QK += mask[None, None, :, :]
-  QK = torch.cat([QK, S], dim=-1)
-  W = torch.softmax(QK, dim=-1)
+  QK = QK.cat(S, dim=-1)
+  W = QK.softmax(axis=-1)
   W = W[..., :-1]
-  attn = torch.einsum("hmqk,khmd->qhmd", W, V)
+  attn = Tensor.einsum("hmqk,khmd->qhmd", W, V)
   return attn.reshape(n_tokens, -1)
 
 
-class AttentionBlock(torch.nn.Module):
+class AttentionBlock:
   def __init__(
     self,
     config: ModelConfig,
     layer_idx: int = 0,
-    device: torch.device | None = None,
   ):
     super().__init__()
     self.head_dim = config.head_dim
@@ -169,29 +150,25 @@ class AttentionBlock(torch.nn.Module):
     self.num_key_value_heads = config.num_key_value_heads
     # Only apply sliding window to every other layer
     self.sliding_window = config.sliding_window if layer_idx % 2 == 0 else 0
-    self.sinks = torch.nn.Parameter(torch.empty(config.num_attention_heads, device=device, dtype=torch.bfloat16))
-    self.norm = RMSNorm(config.hidden_size, device=device)
+    self.sinks = torch.nn.Parameter(torch.empty(config.num_attention_heads))
+    self.norm = nn.RMSNorm(config.hidden_size)
     qkv_dim = config.head_dim * (config.num_attention_heads + 2 * config.num_key_value_heads)
-    self.qkv = torch.nn.Linear(config.hidden_size, qkv_dim, device=device, dtype=torch.bfloat16)
-    self.out = torch.nn.Linear(
+    self.qkv = nn.Linear(config.hidden_size, qkv_dim)
+    self.out = nn.Linear(
       config.head_dim * config.num_attention_heads,
       config.hidden_size,
-      device=device,
-      dtype=torch.bfloat16,
     )
     self.sm_scale = 1 / math.sqrt(config.head_dim)
     self.rope = RotaryEmbedding(
       config.head_dim,
-      config.rope_theta,
-      torch.float32,
+      int(config.rope_theta),
       initial_context_length=config.initial_context_length,
       scaling_factor=config.rope_scaling_factor,
       ntk_alpha=config.rope_ntk_alpha,
       ntk_beta=config.rope_ntk_beta,
-      device=device,
     )
 
-  def forward(self, x: torch.Tensor) -> torch.Tensor:
+  def forward(self, x: Tensor) -> Tensor:
     t = self.norm(x)
     qkv = self.qkv(t)
     q = qkv[:, : self.num_attention_heads * self.head_dim].contiguous()
@@ -230,19 +207,18 @@ def swiglu(x, alpha: float = 1.702, limit: float = 7.0):
   return out_glu * (x_linear + 1)
 
 
-class MLPBlock(torch.nn.Module):
+class MLPBlock:
   def __init__(
     self,
     config: ModelConfig,
-    device: torch.device | None = None,
   ):
     super().__init__()
     self.num_experts = config.num_experts
     self.experts_per_token = config.experts_per_token
     self.swiglu_limit = config.swiglu_limit
     self.world_size = dist.get_world_size() if dist.is_initialized() else 1
-    self.norm = RMSNorm(config.hidden_size, device=device)
-    self.gate = torch.nn.Linear(config.hidden_size, config.num_experts, device=device, dtype=torch.bfloat16)
+    self.norm = nn.RMSNorm(config.hidden_size)
+    self.gate = nn.Linear(config.hidden_size, config.num_experts, device=device, dtype=torch.bfloat16)
     assert config.intermediate_size % self.world_size == 0
     self.mlp1_weight = torch.nn.Parameter(
       torch.empty(
@@ -281,7 +257,7 @@ class MLPBlock(torch.nn.Module):
       )
     )
 
-  def forward(self, x: torch.Tensor) -> torch.Tensor:
+  def forward(self, x: Tensor) -> Tensor:
     t = self.norm(x)
     g = self.gate(t)
     experts = torch.topk(g, k=self.experts_per_token, dim=-1, sorted=True)
@@ -291,36 +267,35 @@ class MLPBlock(torch.nn.Module):
     # MLP #1
     mlp1_weight = self.mlp1_weight[expert_indices, ...]
     mlp1_bias = self.mlp1_bias[expert_indices, ...]
-    t = torch.einsum("beck,bk->bec", mlp1_weight, t) + mlp1_bias
+    t = Tensor.einsum("beck,bk->bec", mlp1_weight, t) + mlp1_bias
     t = swiglu(t, limit=self.swiglu_limit)
 
     # MLP #2
     mlp2_weight = self.mlp2_weight[expert_indices, ...]
     mlp2_bias = self.mlp2_bias[expert_indices, ...]
-    t = torch.einsum("beck,bek->bec", mlp2_weight, t)
+    t = Tensor.einsum("beck,bek->bec", mlp2_weight, t)
     if self.world_size > 1:
       dist.all_reduce(t, op=dist.ReduceOp.SUM)
     t += mlp2_bias
 
     # Weighted sum of experts
-    t = torch.einsum("bec,be->bc", t, expert_weights)
+    t = Tensor.einsum("bec,be->bc", t, expert_weights)
 
     return x + t
 
 
-class TransformerBlock(torch.nn.Module):
+class TransformerBlock:
   def __init__(
     self,
     config: ModelConfig,
     layer_idx: int,
-    device: torch.device | None = None,
   ):
     super().__init__()
     self.layer_idx = layer_idx
-    self.attn = AttentionBlock(config, layer_idx, device)
-    self.mlp = MLPBlock(config, device)
+    self.attn = AttentionBlock(config, layer_idx)
+    self.mlp = MLPBlock(config)
 
-  def forward(self, x: torch.Tensor) -> torch.Tensor:
+  def forward(self, x: Tensor) -> Tensor:
     x = self.attn(x)
     x = self.mlp(x)
     return x
@@ -332,15 +307,13 @@ class Transformer:
     config: ModelConfig,
   ):
     super().__init__()
-    self.embedding = torch.nn.Embedding(config.vocab_size, config.hidden_size, device=device, dtype=torch.bfloat16)
-    self.block = torch.nn.ModuleList([TransformerBlock(config, layer_idx, device) for layer_idx in range(config.num_hidden_layers)])
-    self.norm = RMSNorm(config.hidden_size, device=device)
-    self.unembedding = torch.nn.Linear(
+    self.embedding = nn.Embedding(config.vocab_size, config.hidden_size)
+    self.block = torch.nn.ModuleList([TransformerBlock(config, layer_idx) for layer_idx in range(config.num_hidden_layers)])
+    self.norm = nn.RMSNorm(config.hidden_size)
+    self.unembedding = nn.Linear(
       config.hidden_size,
       config.vocab_size,
       bias=False,
-      device=device,
-      dtype=torch.bfloat16,
     )
 
   def forward(self, x: Tensor) -> Tensor:
@@ -446,7 +419,7 @@ if __name__ == "__main__":
 
   model = Transformer(model_config)
 
-  nn.state.load_state_dict(model, state_dict, verbose=False, consume=True, realize=False)  # NOTE: rope_freqs.weight (32,) is unused
+  nn.state.load_state_dict(model, state_dict)
 
   # extract some metadata
   tok = SimpleTokenizer.from_gguf_kv(kv)
