61060473a4f25c8b6f1e1d285ea4659582009688
Caleb Ouellette <caleb.ouellette@gmail.com>
2025-11-06 20:12:23 -0800

    WIP loading.

--- tinygrad/apps/open-ai-oss.py
+++ tinygrad/apps/open-ai-oss.py
@@ -5,7 +5,69 @@ from dataclasses import dataclass
 from typing import Tuple
 import sys, argparse
 from tinygrad import Tensor, nn, UOp, TinyJit, getenv
-from .llm import SimpleTokenizer
+
+
+class SimpleTokenizer:
+  def __init__(self, normal_tokens: dict[str, int], special_tokens: dict[str, int]):
+    # https://github.com/openai/gpt-2/blob/9b63575ef42771a015060c964af2c3da4cf7c8ab/src/encoder.py#L9
+    bs = [*range(33, 127), *range(161, 173), *range(174, 256)]  # bytes that map to themselves
+    self._byte_decoder = {chr(b): b for b in bs} | {chr(256 + i): b for i, b in enumerate(b for b in range(256) if b not in bs)}
+
+    # https://github.com/ggml-org/llama.cpp/blob/94933c8c2eeaa9a7983e3f6c08af76bd86724094/src/llama-vocab.cpp#L286
+    def ucat_range(pre: str):
+      return "".join(re.escape(chr(cp)) for cp in range(sys.maxunicode + 1) if unicodedata.category(chr(cp)).startswith(pre))
+
+    r_ws, r_p_N, r_p_L = r"\t\n\x0b\x0c\r\x85" + ucat_range("Z"), ucat_range("N"), ucat_range("L")
+    self._split_to_word = re.compile(
+      "(?i:'s|'t|'re|'ve|'m|'ll|'d)|"
+      + f"[^\\r\\n{r_p_N}{r_p_L}]?[{r_p_L}]+|[{r_p_N}]{{1,3}}| ?[^{r_ws}{r_p_N}{r_p_L}]+[\\r\\n]*|[{r_ws}]*[\\r\\n]+|[{r_ws}]+(?![^{r_ws}])|[{r_ws}]+"
+    )
+    self._split_to_sentence = re.compile("|".join(re.escape(tok) for tok in special_tokens.keys()) if special_tokens else r"(?!)")
+
+    self._normal_tokens = {bytes(self._byte_decoder[c] for c in tok): tid for tok, tid in normal_tokens.items()}
+    self._special_tokens = special_tokens
+    self._tok2bytes = {tid: tok for tok, tid in self._normal_tokens.items()} | {tid: tok.encode() for tok, tid in self._special_tokens.items()}
+
+  @staticmethod
+  def from_gguf_kv(kv: dict):
+    # https://github.com/ggml-org/llama.cpp/blob/94933c8c2eeaa9a7983e3f6c08af76bd86724094/src/llama-vocab.cpp#L1818-L1820
+    # if kv["tokenizer.ggml.pre"] not in ("llama3", "llama-v3", "llama-bpe"):
+    # raise ValueError(f"Invalid tokenizer preset '{kv['tokenizer.ggml.pre']}'")
+    vocab: typing.Iterable[tuple[str, int]] = ((tok, idx) for idx, tok in enumerate(kv["tokenizer.ggml.tokens"]))
+    normal_tokens, special_tokens = helpers.partition(vocab, lambda e: kv["tokenizer.ggml.token_type"][e[1]] == 1)
+    return SimpleTokenizer(dict(normal_tokens), dict(special_tokens))
+
+  def _encode_word(self, word: bytes) -> list[int]:
+    if (early_token := self._normal_tokens.get(word)) is not None:
+      return [early_token]
+    parts = [bytes([b]) for b in word]
+    # greedily merge any parts that we can
+    while True:
+      i = min([(sys.maxsize, -1)] + [(self._normal_tokens.get(parts[j] + parts[j + 1], sys.maxsize), j) for j in range(len(parts) - 1)])[1]
+      if i == -1:
+        break
+      parts[i : i + 2] = [parts[i] + parts[i + 1]]
+    try:
+      return [self._normal_tokens[p] for p in parts]
+    except KeyError:
+      raise RuntimeError("token not found")
+
+  def _encode_sentence(self, chunk: str) -> list[int]:
+    return [tok for word in self._split_to_word.findall(chunk) for tok in self._encode_word(word.encode())]
+
+  def encode(self, text: str) -> list[int]:
+    tokens: list[int] = []
+    pos = 0
+    for match in self._split_to_sentence.finditer(text):
+      tokens.extend(self._encode_sentence(text[pos : match.start(0)]) + [self._special_tokens[text[match.start(0) : match.end(0)]]])
+      pos = match.end(0)
+    return tokens + self._encode_sentence(text[pos:])
+
+  def decode(self, ids: list[int]) -> str:
+    return b"".join(self._tok2bytes[tid] for tid in ids).decode()
+
+  def role(self, role: str):
+    return self.encode("<|start_header_id|>" + role + "<|end_header_id|>\n\n")
 
 
 @dataclass
@@ -248,14 +310,14 @@ class MLPBlock:
     self.experts_per_token = config.experts_per_token
     self.swiglu_limit = config.swiglu_limit
     self.norm = nn.RMSNorm(config.hidden_size)
-    self.gate = nn.Linear(config.hidden_size, config.num_experts, device=device, dtype=torch.bfloat16)
+    self.gate = nn.Linear(config.hidden_size, config.num_experts)
 
     self.mlp1_weight = Tensor.zeros(
       config.num_experts,
       config.intermediate_size * 2,
       config.hidden_size,
     )
-    self.mlp1_bias = Tensor.zeros(config.num_experts, config.intermediate_size * 2 // self.world_size)
+    self.mlp1_bias = Tensor.zeros(config.num_experts, config.intermediate_size * 2)
 
     self.mlp2_weight = Tensor.zeros(
       config.num_experts,
@@ -314,8 +376,8 @@ class Transformer:
     config: ModelConfig,
   ):
     super().__init__()
-    self.embedding = nn.Embedding(config.vocab_size, config.hidden_size)
-    self.block = [TransformerBlock(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
+    self.token_embd = nn.Embedding(config.vocab_size, config.hidden_size)
+    self.blk = [TransformerBlock(config, layer_idx) for layer_idx in range(config.num_hidden_layers)]
     self.norm = nn.RMSNorm(config.hidden_size)
     self.unembedding = nn.Linear(
       config.hidden_size,
@@ -430,6 +492,17 @@ models = {
   "20B": "https://huggingface.co/bartowski/openai_gpt-oss-20b-GGUF/resolve/main/openai_gpt-oss-20b-Q6_K.gguf",
 }
 
+
+def rename_state_dict_keys(state_dict: dict, kv: dict) -> dict:
+  for i in range(0, kv['gpt-oss.block_count']):
+    # attention
+    state_dict[f'blk.{i}.attn.sinks'] = state_dict.pop(f'blk.{i}.attn_sinks.weight')
+    state_dict[f'blk.{i}.attn.norm.weight'] = state_dict.pop(f'blk.{i}.attn_norm.weight')
+    state_dict[f'blk.{i}.attn.qkv.weight'] = state_dict.pop(f'blk.{i}.attn_qkv.weight')
+
+  return state_dict
+
+
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
   parser.add_argument("--size", choices=list(models.keys()), default=list(models.keys())[0], help="Model size")
@@ -444,7 +517,7 @@ if __name__ == "__main__":
 
   model = Transformer(model_config)
 
-  nn.state.load_state_dict(model, state_dict)
+  nn.state.load_state_dict(model, rename_state_dict_keys(state_dict, kv))
 
   # extract some metadata
   tok = SimpleTokenizer.from_gguf_kv(kv)
